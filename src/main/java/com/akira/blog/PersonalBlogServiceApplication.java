7c2507540cd03e27eaaacf7d16d3021e0bde31c1
Akira <akira0x01@gmail.com>
2024-05-23 15:48:45 +0800

    init

---
+++ src/main/java/com/akira/blog/PersonalBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akira.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalBlogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akira/blog/PersonalBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akira.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
